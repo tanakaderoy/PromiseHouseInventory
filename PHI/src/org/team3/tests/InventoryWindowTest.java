e6ff38e76308116a1e6e34566e9bfdc953819ead
JakeCarney103 <jakecarney103@gmail.com>
2019-03-21 10:59:34 -0400

    Added test folder with

---
+++ PHI/src/org/team3/tests/InventoryWindowTest.java
@@ -0,0 +1,33 @@
+package org.team3.tests;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InventoryWindowTest {
+
+	@Test
+	public void testItemList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInventoryWindow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInventoryWindow1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewFilter() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PHI/src/org/team3/tests/RunAllTests.java
@@ -0,0 +1,15 @@
+package org.team3.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	/*INSERT TESTS HERE*/
+	AddWindowTest.class,
+	InventoryWindowTest.class
+})
+
+public class RunAllTests {
+
+}
