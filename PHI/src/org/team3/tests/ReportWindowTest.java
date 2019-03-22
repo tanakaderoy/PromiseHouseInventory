61a804e2e7eb2bdfe7e8a52674aa905f4dabbd87
Hao <hao.zhou@otterbein.edu>
2019-03-22 13:31:11 -0400

    ReportWindow tests

---
+++ PHI/src/org/team3/tests/ReportWindowTest.java
@@ -0,0 +1,30 @@
+package org.team3.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.team3.PHI.Item;
+
+public class ReportWindowTest {
+	
+	Item item = new Item(12345,"Doritos",3.00,9,"chips","2019-03-19");
+	Item text = new Item(123, "Drink", null, 0, null, null);
+	
+	@Test
+	public void testItemList() {
+		assertNotNull(item);;
+	}
+
+	@Test
+	public void testShowItem() {
+		assertEquals(item, item);;
+	}
+
+
+	@Test
+	public void testFilterItemList() {
+		assertEquals(text, text);
+	}
+
+
+}
--- PHI/src/org/team3/tests/RunAllTests.java
+++ PHI/src/org/team3/tests/RunAllTests.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite;
 	AddWindowTest.class,
 	InventoryWindowTest.class,
 	MainPHITest.class,
-	ItemTest.class
+	ItemTest.class,
+	ReportWindowTest.class
 })
 
 public class RunAllTests {
