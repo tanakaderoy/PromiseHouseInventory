7f2b0850f826c5f1de73c076c37455e28108e356
mattchu2521 <42874588+mattchu2521@users.noreply.github.com>
2019-03-23 17:49:55 -0700

    Table column adjuster test

---
+++ PHI/src/org/team3/tests/TableColumnAdjusterTest.java
@@ -0,0 +1,28 @@
+package org.team3.tests;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JTable;
+
+import org.junit.Test;
+import org.team3.PHI.TableColumnAdjuster;
+
+import junit.framework.Assert;
+
+public class TableColumnAdjusterTest {
+	JTable tester = new JTable(3,3);
+	TableColumnAdjuster test = new TableColumnAdjuster(tester);
+	
+	@Test
+	public void testgetColumnWidth() {
+		Assert.assertEquals(10, test.getColumnHeaderWidth(1));
+	}
+	@Test
+	public void testAdjustColumn() {
+		test.adjustColumn(1);
+		Assert.assertEquals(10, test.getColumnHeaderWidth(1));
+	}
+
+	
+	
+}
