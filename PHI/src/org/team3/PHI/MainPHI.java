3fad990055f497b002621348026e197272158f7c
tanakaderoy <tanaka,deroy@gmail.com>
2019-03-23 22:09:26 -0400

    tests

--- PHI/src/org/team3/PHI/MainPHI.java
+++ PHI/src/org/team3/PHI/MainPHI.java
@@ -9,7 +9,7 @@ public class MainPHI {
 	public static void main(String[] args) {
 		
 		try {
-            // Set System Look&Feel
+            // Set System L&F
         UIManager.setLookAndFeel(
             UIManager.getSystemLookAndFeelClassName());
     } 
